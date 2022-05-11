ace792b701bd2e21c7cd534fec17d267723c1870
hayatog <guilherme.ryders@gmail.com>
2022-05-10 22:19:35 -0300

    Adicionando persistencia com Room

--- app/src/main/java/br/senac/noteapp/activities/ListNotesActivity.kt
+++ app/src/main/java/br/senac/noteapp/activities/ListNotesActivity.kt
@@ -1,8 +1,13 @@
 package br.senac.noteapp.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room
+import br.senac.noteapp.data.AppDatabase
+import br.senac.noteapp.data.Note
 import br.senac.noteapp.databinding.ActivityListNotesBinding
+import br.senac.noteapp.databinding.CardNoteBinding
 
 class ListNotesActivity : AppCompatActivity() {
     lateinit var binding: ActivityListNotesBinding
@@ -12,6 +17,40 @@ class ListNotesActivity : AppCompatActivity() {
         binding = ActivityListNotesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Intent para ir de uma tela para a outra
+        binding.fab.setOnClickListener {
+            val i = Intent(this, NewNoteActivity::class.java)
+            startActivity(i)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateNotes()
+    }
+
+    fun updateNotes() {
+        Thread {
+            val db = Room.databaseBuilder(this, AppDatabase::class.java, "db").build()
+            val list = db.noteDao().listAll()
+            runOnUiThread {
+                updateUi(list)
+            }
+        }.start()
+    }
+
+    fun updateUi(list: List<Note>) {
+
+        binding.container.removeAllViews()
+
+        list.forEach {
+            val card = CardNoteBinding.inflate(layoutInflater)
+
+            card.textTitle.text = it.title
+            card.textDesc.text = it.desc
+
+            binding.container.addView(card.root)
+        }
     }
 
 }
--- app/src/main/java/br/senac/noteapp/activities/NewNoteActivity.kt
+++ app/src/main/java/br/senac/noteapp/activities/NewNoteActivity.kt
@@ -2,6 +2,10 @@ package br.senac.noteapp.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room
+import br.senac.noteapp.data.AppDatabase
+import br.senac.noteapp.data.Note
+import br.senac.noteapp.data.Notes
 import br.senac.noteapp.databinding.ActivityNewNoteBinding
 
 class NewNoteActivity : AppCompatActivity() {
@@ -12,6 +16,27 @@ class NewNoteActivity : AppCompatActivity() {
         binding = ActivityNewNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        binding.btnAdd.setOnClickListener {
+
+            val note = Note(title = binding.etTitle.text.toString(), desc = binding.etDesc.text.toString())
+
+            //Notes.listNotes.add(note)
+            Thread {
+                insertNote(note)
+                finish()
+            }.start()
+
+
+        }
+
     }
+
+     fun insertNote(note: Note) {
+         val db = Room.databaseBuilder(this, AppDatabase::class.java, "db").build()
+
+         db.noteDao().insert(note)
+
+     }
 
 }
